00ae7d3371ffae78850641517a7bbf5d80b4d1a7
shu_0115 <raisondetre0115@gmail.com>
2012-05-12 20:34:24 +0900

    update

--- lib/generators/paypal/scaffold/scaffold_generator.rb
+++ lib/generators/paypal/scaffold/scaffold_generator.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'rails/generators'
 
 module Paypal
@@ -14,7 +15,17 @@ module Paypal
         # Config
         if File.exist?('config/initializers/local_setting.rb')
 #          insert_into_file "app/assets/javascripts/application.js", "//= require twitter/bootstrap\n", :after => "jquery_ujs\n"
-          append_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
+#          append_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
+          append_file 'config/initializers/local_setting.rb' do
+            '# PayPal'
+            'ENV["PAYPAL_SANDBOX"]   = "ON"'
+            'ENV["PAYPAL_USER_NAME"] = "test01_1336296393_biz_api1.gmail.com"'
+            'ENV["PAYPAL_PASSWORD"]  = "1336296414"'
+            'ENV["PAYPAL_SIGNATURE"] = "AiPC9BjkCyDFQXbSkoZcgqH3hpacAzpTbNTAkYEP8T8QC6kv0aF-gRj-"'
+            'ENV["PAYPAL_PERIOD"]    = "Month"  # 周期 ie.) :Month, :Week, :Day'
+            'ENV["PAYPAL_FREQUENCY"] = "1"       # 回数'
+            'ENV["PAYPAL_AMOUNT"]    = "150"     # 金額'
+          end
         else
 #          copy_file "application.js", "app/assets/javascripts/application.js"
           copy_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
