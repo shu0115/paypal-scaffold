7c08c6736fd2fb34b78d88d915d1b76084ec9a05
shu_0115 <raisondetre0115@gmail.com>
2012-05-12 20:55:29 +0900

    update

--- lib/generators/paypal/scaffold/scaffold_generator.rb
+++ lib/generators/paypal/scaffold/scaffold_generator.rb
@@ -19,7 +19,7 @@ module Paypal
           
 #          insert_into_file "config/initializers/local_setting.rb", content.to_s
           insert_into_file "config/initializers/local_setting.rb", "# PayPal\n"
-          insert_into_file "config/initializers/local_setting.rb", "ENV['PAYPAL_SANDBOX']   = \"ON\"\n"
+#          insert_into_file "config/initializers/local_setting.rb", "ENV['PAYPAL_SANDBOX']   = \"ON\"\n"
           
 #          insert_into_file "app/assets/javascripts/application.js", "//= require twitter/bootstrap\n", :after => "jquery_ujs\n"
 #          append_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
