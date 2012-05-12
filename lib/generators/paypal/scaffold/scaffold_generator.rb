77e8379bc167a2921c449bee49096f7d321b4429
shu_0115 <raisondetre0115@gmail.com>
2012-05-12 21:49:59 +0900

    update

--- lib/generators/paypal/scaffold/scaffold_generator.rb
+++ lib/generators/paypal/scaffold/scaffold_generator.rb
@@ -14,8 +14,14 @@ module Paypal
         
         # Config
         if File.exist?('config/initializers/local_setting.rb')
-          content = "# PayPal\n"
+          content = "\n# PayPal\n"
           content += "ENV['PAYPAL_SANDBOX']   = \"ON\"\n"
+          content += "ENV['PAYPAL_USER_NAME'] = \"test01_1336296393_biz_api1.gmail.com\"\n"
+          content += "ENV['PAYPAL_PASSWORD']  = \"1336296414\"\n"
+          content += "ENV['PAYPAL_SIGNATURE'] = \"AiPC9BjkCyDFQXbSkoZcgqH3hpacAzpTbNTAkYEP8T8QC6kv0aF-gRj-\"\n"
+          content += "ENV['PAYPAL_PERIOD']    = \"Month\"  # 周期 ie.) :Month, :Week, :Day\n"
+          content += "ENV['PAYPAL_FREQUENCY'] = \"1\"       # 回数\n"
+          content += "ENV['PAYPAL_AMOUNT']    = \"150\"     # 金額\n"
           
           append_file "config/initializers/local_setting.rb", content
           
