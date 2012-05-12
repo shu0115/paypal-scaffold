992af6e65d629b939b907ca9e722bcdbdb299dd8
shu_0115 <raisondetre0115@gmail.com>
2012-05-12 20:38:57 +0900

    update

--- lib/generators/paypal/scaffold/templates/config/initializers/local_setting.rb
+++ lib/generators/paypal/scaffold/templates/config/initializers/local_setting.rb
@@ -4,5 +4,5 @@ ENV['PAYPAL_USER_NAME'] = "test01_1336296393_biz_api1.gmail.com"
 ENV['PAYPAL_PASSWORD']  = "1336296414"
 ENV['PAYPAL_SIGNATURE'] = "AiPC9BjkCyDFQXbSkoZcgqH3hpacAzpTbNTAkYEP8T8QC6kv0aF-gRj-"
 ENV['PAYPAL_PERIOD']    = "Month"  # 周期 ie.) :Month, :Week, :Day
-ENV['PAYPAL_FREQUENCY'] = "1"       # 回数
-ENV['PAYPAL_AMOUNT']    = "150"     # 金額
+ENV['PAYPAL_FREQUENCY'] = "1"      # 回数
+ENV['PAYPAL_AMOUNT']    = "150"    # 金額
